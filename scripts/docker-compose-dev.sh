937ffb23dc780ce9a77052c5e9459e987e2a2fc0
phunpt <phu.npt01@gmail.com>
2025-12-28 19:28:19 +0700

    update docker compose dev, test and scripts

--- scripts/docker-compose-dev.sh
+++ scripts/docker-compose-dev.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # Start ML API for development
+if ! docker network ls --format '{{.Name}}' | grep -q '^app-shared-network-dev$'; then
+    docker network create --driver bridge --attachable app-shared-network-dev
+fi
 docker compose -f docker-compose.dev.yaml --env-file .env.dev -p ml-api-dev up -d
